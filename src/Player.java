e815f91f72895941433c8e11724caabe172a9d64
emoly <emdormody@mun.ca>
2023-03-11 10:30:06 -0330

    started choose player

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,6 @@
 public class Player {
-
+    private PlayerDetails details;
     public Player(PlayerDetails details){
-
+        this.details = details;
     }
 }
--- src/PlayerDetails.java
+++ src/PlayerDetails.java
@@ -1,2 +1,58 @@
 public class PlayerDetails {
+    private String name;
+    private int strength, speed, intelligence, strategy, luck, health;
+    public PlayerDetails(){
+        health = 100;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength){
+        this.strength = strength;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setStrategy(int strategy) {
+        this.strategy = strategy;
+    }
+
+    public int getLuck() {
+        return luck;
+    }
+
+    public void setLuck(int luck) {
+        this.luck = luck;
+    }
+
+    public int getStrategy() {
+        return strategy;
+    }
 }
